c0d515c415cc721ff07f3dd964612380a2580062
Eric Stern <eric@ericstern.com>
2022-04-11 11:27:28 -0700

    Implement actual logic

--- check.php
+++ check.php
@@ -2,9 +2,57 @@
 
 declare(strict_types=1);
 
-ksort($_ENV);
-print_r($_ENV);
-ksort($_SERVER);
-print_r($_SERVER);
+use RecursiveDirectoryIterator as RDI;
 
-print_r($argv);
+function debug(string $message)
+{
+    echo "::debug::$message\n";
+}
+
+// Create a map of normalized file extensions
+$extensionsToCheck = array_flip(array_map('strtolower', array_map('trim', explode(',', getenv('INPUT_FILE_EXTENSIONS')))));
+
+debug("File extensions: " . implode(', ', array_keys($extensionsToCheck)));
+
+// This assumes that the action doesn't do anything too silly
+$rdi = new RecursiveDirectoryIterator('.', RDI::SKIP_DOTS);
+$rii = new RecursiveIteratorIterator($rdi);
+
+$exit = 0;
+
+foreach ($rii as $file => $fileinfo) {
+    if (!array_key_exists($fileinfo->getExtension(), $extensionsToCheck)) {
+        debug("Skipping $file");
+        continue;
+    }
+
+    $command = sprintf('php -l %s 2>&1', escapeshellarg($file));
+    $output = []; // Must reset inside each loop; exec appends rather than sets
+    $ret = exec($command, $output, $exitCode);
+    if ($exitCode === 0) {
+        // Lint OK - immediately move to the next file.
+        continue;
+    }
+
+    $exit = 1;
+
+    // Try to parse the output for more detailed info
+    $printed = false;
+    foreach ($output as $line) {
+        // Something like this:
+        // "Parse error: syntax error, unexpected token "private", expecting "{" in src/someFile.php on line 23"
+        $matched = preg_match("/Parse error:\s+(?'text'.*) in (?'file'.*) on line (?'line'\d+)$/", $line, $matches);
+        if ($matched) {
+            echo "::error file=$file,line={$matches['line']}::{$matches['text']}\n";
+            $printed = true;
+            break;
+        }
+    }
+    if (!$printed) {
+        // Fallback error format
+        echo "::error file=$file::Syntax error\n";
+    }
+
+}
+
+exit($exit);
